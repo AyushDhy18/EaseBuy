509498710bcf16f7043e6db76d5060a0196647b8
Ayussh Codeyogi <ayushdhyani1875@gmail.com>
2022-09-18 12:11:48 +0530

    not found image created

--- src/NotFound.jsx
+++ src/NotFound.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import NotFoundImage from "./not_found_image.jpg";
 
 function NotFound() {
   return (
     <div className="flex items-center justify-center flex-col">
-      <img
-        className="w-1/2 h-1/2"
-        src="https://media.istockphoto.com/photos/error-message-picture-id485435279?b=1&k=20&m=485435279&s=170667a&w=0&h=FVYP9WC3Bfwk9V8ERWHJJdDe7h6i5zF3oJpNqa6qn2c="
-      />
+      <img className="w-1/2 h-1/2" src={NotFoundImage} />
       <h1 className="text-3xl text-indigo-500">Page Not Found</h1>
       <h2 className=" text-gray-400">
         Probably error occured due to wrong url
