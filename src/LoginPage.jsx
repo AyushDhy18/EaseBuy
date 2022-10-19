3f1f7c10da3ef72bfc0c1380cc845549f8964649
Ayussh Codeyogi <ayushdhyani1875@gmail.com>
2022-10-19 18:01:26 +0530

    improved login and signup page using withformik

--- src/LoginPage.jsx
+++ src/LoginPage.jsx
@@ -1,85 +1,106 @@
 import React from "react";
-import Button from "./Button";
 import * as Yup from "yup";
 import { Link } from "react-router-dom";
-import { FormikInput } from "./Input";
-import { Formik, Form } from "formik";
+import { withFormik } from "formik";
+import Input from "./Input";
 
-const LoginPage = () => {
-  const callLoginApi = () => {
-    console.log(
-      "Sending Data",
-      "email:",
-      value.email,
-      "password:",
-      value.password
-    );
-  };
-  const schema = Yup.object().shape({
-    email: Yup.string().email("Please enter a valid email"),
-    password: Yup.string().min(8).max(20),
-  });
+const callLoginApi = () => {
+  console.log(
+    "Sending Data",
+    "email:",
+    values.email,
+    "password:",
+    values.password
+  );
+};
+const schema = Yup.object().shape({
+  email: Yup.string().email("Please enter a valid email"),
+  password: Yup.string().min(8).max(20),
+});
 
-  const initialValues = {
-    email: "",
-    password: "",
-  };
+const initialValues = {
+  email: "",
+  password: "",
+};
+export const LoginPage = ({
+  handleSubmit,
+  errors,
+  handleBlur,
+  values,
+  touched,
+  handleChange,
+}) => {
+  console.log(values);
   return (
     <>
       <div>
         <div className="flex justify-center items-center h-screen w-screen">
-          <Formik
-            initialValues={initialValues}
-            validationSchema={schema}
-            validateOnMount
-            onSubmit={callLoginApi}
+          <form
+            onSubmit={handleSubmit}
+            className="bg-blue-200 border-2 shadow-lg shadow-blue-300 p-3 w-2/3"
           >
-            <Form className="bg-blue-200 border-2 shadow-lg shadow-blue-300 p-3 w-2/3">
-              <h1 className="text-2xl font-bold self-start mb-4">Login</h1>
+            <h1 className="text-2xl font-bold self-start mb-4">Login</h1>
 
-              <FormikInput
-                label="Email Address"
-                id="email-address"
-                type="email"
-                name="email"
-                required
-                className=""
-              />
+            <Input
+              error={errors.email}
+              values={values.email}
+              touched={touched.email}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              label="Email Address"
+              id="email-address"
+              type="email"
+              name="email"
+              required
+              className=""
+            />
 
-              <FormikInput
-                label="password"
-                id="password"
-                type="password"
-                name="password"
-                required
-                className=""
-              />
+            <Input
+              error={errors.password}
+              values={values.password}
+              touched={touched.password}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              label="password"
+              id="password"
+              type="password"
+              name="password"
+              required
+              className=""
+            />
 
-              <div className="flex justify-end\">
-                <Link to="/My-Account/Forgot-password" className="text-red-500">
-                  Forgot password ?
-                </Link>
-                <div>
-                  <button
-                    type="submit"
-                    className="mt-3 rounded-lg border-2 bg-red-500 px-4 py-1 text-white  disabled:bg-red-300 "
-                  >
-                    Login
-                  </button>
-                </div>
+            <div className="flex justify-end\">
+              <Link to="/My-Account/Forgot-password" className="text-red-500">
+                Forgot password ?
+              </Link>
+              <div>
+                <button
+                  type="submit"
+                  className="mt-3 rounded-lg border-2 bg-red-500 px-4 py-1 text-white  disabled:bg-red-300 "
+                >
+                  Login
+                </button>
               </div>
-              <div className="flex justify-center">
-                <h1 className="text-blue-600">Don't have an Account ? </h1>
-                <Link to="/My-Account/SignUp" className="text-red-500">
-                  {" "}
-                  Signup
-                </Link>
-              </div>
-            </Form>
-          </Formik>
+            </div>
+            <div className="flex justify-center">
+              <h1 className="text-blue-600">Don't have an Account ? </h1>
+              <Link to="/My-Account/SignUp" className="text-red-500">
+                {" "}
+                Signup
+              </Link>
+            </div>
+          </form>
         </div>
       </div>
     </>
   );
 };
-export default LoginPage;
+const myHOC = withFormik({
+  initialValues: initialValues,
+  validationSchema: schema,
+  handleSubmit: callLoginApi,
+});
+
+const easyLogin = myHOC(LoginPage);
+
+export default easyLogin;
--- src/SignUpPage.jsx
+++ src/SignUpPage.jsx
@@ -1,114 +1,149 @@
-import { Formik, Form } from "formik";
+import { withFormik } from "formik";
 import React from "react";
-import Button from "./Button";
 import * as Yup from "yup";
 import { Link } from "react-router-dom";
-import { FormikInput } from "./Input";
+import Input from "./Input";
 
-const SignUpPage = () => {
-  const callLoginApi = () => {
-    console.log(
-      "Sending Data",
-      "email:",
-      values.email,
-      "password:",
-      values.password
-    );
-  };
-  const schema = Yup.object().shape({
-    email: Yup.string().required().email("Please enter a valid email"),
-    password: Yup.string().required().min(8).max(20),
-    UserName: Yup.string().required().min(10).max(25),
-    Fullname: Yup.string().required(),
-    ConfirmPassword: Yup.string().required(),
-  });
+const callLoginApi = () => {
+  console.log(
+    "Sending Data",
+    "email:",
+    values.email,
+    "password:",
+    values.password
+  );
+};
+const schema = Yup.object().shape({
+  email: Yup.string().required().email("Please enter a valid email"),
+  password: Yup.string().required().min(8).max(20),
+  UserName: Yup.string().required().min(10).max(25),
+  Fullname: Yup.string().required(),
+  ConfirmPassword: Yup.string().required(),
+});
 
-  const initialValues = {
-    email: "",
-    password: "",
-    UserName: "",
-    Fullname: "",
-    ConfirmPassword: "",
-  };
+const initialValues = {
+  email: "",
+  password: "",
+  UserName: "",
+  Fullname: "",
+  ConfirmPassword: "",
+};
+export const SignUpPage = ({
+  handleSubmit,
+  errors,
+  handleBlur,
+  values,
+  touched,
+  handleChange,
+}) => {
   return (
     <>
       <div>
         <div className="flex justify-center items-center h-screen w-screen">
-          <Formik
-            initialValues={initialValues}
-            validationSchema={schema}
-            validateOnMount
-            onSubmit={callLoginApi}
+          <form
+            onSubmit={handleSubmit}
+            className="bg-blue-200 border-2 shadow-lg shadow-blue-300 p-3 w-2/3"
           >
-            <Form className="bg-blue-200 border-2 shadow-lg shadow-blue-300 p-3 w-2/3">
-              <h1 className="text-xl font-bold self-start mb-4">Sign Up</h1>
+            <h1 className="text-xl font-bold self-start mb-4">Sign Up</h1>
 
-              <FormikInput
-                label="Fullname"
-                id="Fullname"
-                type="text"
-                name="Fullname"
-                required
-                className=""
-              />
+            <Input
+              error={errors.Fullname}
+              values={values.Fullname}
+              touched={touched.Fullname}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              label="Fullname"
+              id="Fullname"
+              type="text"
+              name="Fullname"
+              required
+              className=""
+            />
 
-              <FormikInput
-                label="Email Address"
-                id="email-address"
-                type="email"
-                name="email"
-                required
-                className=""
-              />
-              <FormikInput
-                label="UserName"
-                id="UserName"
-                type="text"
-                name="UserName"
-                required
-                className=""
-              />
+            <Input
+              error={errors.email}
+              values={values.email}
+              touched={touched.email}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              label="Email Address"
+              id="email-address"
+              type="email"
+              name="email"
+              required
+              className=""
+            />
+            <Input
+              error={errors.UserName}
+              values={values.UserName}
+              touched={touched.UserName}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              label="UserName"
+              id="UserName"
+              type="text"
+              name="UserName"
+              required
+              className=""
+            />
 
-              <FormikInput
-                label="password"
-                id="password"
-                type="password"
-                name="password"
-                required
-                className=""
-              />
+            <Input
+              error={errors.password}
+              values={values.password}
+              touched={touched.password}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              label="password"
+              id="password"
+              type="password"
+              name="password"
+              required
+              className=""
+            />
 
-              <FormikInput
-                label="ConfirmPassword"
-                id="ConfirmPassword"
-                type="password"
-                name="ConfirmPassword"
-                required
-                className=""
-              />
+            <Input
+              error={errors.ConfirmPassword}
+              values={values.ConfirmPassword}
+              touched={touched.ConfirmPassword}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              label="ConfirmPassword"
+              id="ConfirmPassword"
+              type="password"
+              name="ConfirmPassword"
+              required
+              className=""
+            />
 
-              <div className="flex justify-end">
-                <div>
-                  <button
-                    type="submit"
-                    className="mt-3 rounded-lg border-2 bg-red-500 px-4 py-1 text-white  disabled:bg-red-300 "
-                  >
-                    Sign up
-                  </button>
-                </div>
+            <div className="flex justify-end">
+              <div>
+                <button
+                  type="submit"
+                  className="mt-3 rounded-lg border-2 bg-red-500 px-4 py-1 text-white  disabled:bg-red-300 "
+                >
+                  Sign up
+                </button>
               </div>
-              <div className="flex justify-center">
-                <h1 className="text-blue-600">Already have an Account ? </h1>
-                <Link to="/My-Account/Login" className="text-red-500">
-                  {" "}
-                  Login
-                </Link>
-              </div>
-            </Form>
-          </Formik>
+            </div>
+            <div className="flex justify-center">
+              <h1 className="text-blue-600">Already have an Account ? </h1>
+              <Link to="/My-Account/Login" className="text-red-500">
+                {" "}
+                Login
+              </Link>
+            </div>
+          </form>
         </div>
       </div>
     </>
   );
 };
-export default SignUpPage;
+const myHOC = withFormik({
+  initialValues: initialValues,
+  validationSchema: schema,
+  handleSubmit: callLoginApi,
+});
+
+const easySignUp = myHOC(SignUpPage);
+
+export default easySignUp;
